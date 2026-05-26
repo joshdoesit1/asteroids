5a74410bbe213cf8c2858b7c5b66f093b1e78988
joshdoesit1 <josh@joshdoesit.com>
2026-05-26 15:06:22 -0500

    Fixed gameloop

--- main.py
+++ main.py
@@ -5,23 +5,28 @@ from logger import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        log_state()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT():
-            return
-    fill("black")
-    display.flip()
-
-
-
-
-
-
+    timer = pygame.time.Clock()
+    dt = 0.0
+    
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+
+    while True:
+        log_state()
+        dt = timer.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
+
+
+
     
 if __name__ == "__main__":
     main()
